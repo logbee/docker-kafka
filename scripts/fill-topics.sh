c0fb25ff7f23a21eb8bcb31ef097b029588df899
mlandth <max.landthaler@posteo.de>
2018-05-17 10:52:34 +0200

    fixed syntax in fill-topics script.

--- scripts/fill-topics.sh
+++ scripts/fill-topics.sh
@@ -2,12 +2,12 @@
 
 echo -e "Started topic filling with testdata.\n" >> /var/log/kafka-topics/kafka-topic-creation.log
 
-if [ -n $1 && -n $2 ]; then
+if [ -n $1 ] && [ -n $2 ]; then
     IFS=','; for topicToFill in $1; do
         TOP=$(/opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/bin/kafka-topics.sh --zookeeper localhost:2181 --list | grep -c ${topicToFill})
         if [ $TOP -eq 1 ]; then
             echo -e "[START] Filling ${topicToFill} with testdata..." >> /var/log/kafka-topics/kafka-topic-creation.log
-            /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic ${topicToFill} < /mnt/$2
+            /opt/kafka_${SCALA_VERSION}-${KAFKA_VERSION}/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic ${topicToFill} < $2
         else
             echo -e "[ERROR] ${topicToFill} can not be filled with testdata." >> /var/log/kafka-topics/kafka-topic-creation.log
         fi
